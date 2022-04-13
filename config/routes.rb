162cf05cca98828c12364feb389a0ff205f249c6
izumi-hiro <transformers2728frost@gmail.com>
2022-04-13 05:09:31 +0000

    [Add] bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new' => 'books#new'
+  
   get 'books' => 'books#new'
   post 'books' => 'books#create'
   get 'books/index' => 'books#index'
-  get 'books/show' => 'books#show'
   get 'books/edit' => 'books#edit'
+  get 'books/:id' => 'books#show', as: 'book'
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
