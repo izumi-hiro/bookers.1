0c13752e69c22295e25de8ca905ea3a7b805d4b2
izumi-hiro <transformers2728frost@gmail.com>
2022-04-13 12:55:24 +0000

    [Add] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "succesfully"
       redirect_to book_path(@book.id)
     else
       @books = Book.all
