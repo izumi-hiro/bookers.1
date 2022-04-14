f4b2aec08c67dea03b740b006ac6c883aab9ba23
izumi-hiro <transformers2728frost@gmail.com>
2022-04-14 05:31:38 +0000

    [Finish] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was succesfully created."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render :new
+      render :index
     end
   end
 
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Book was succesfully created."
+      flash[:notice] = "Book was successfully update."
       redirect_to book_path(@book.id)
     else
       render :edit
@@ -39,7 +39,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "Book was succesfully destroyed."
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   
